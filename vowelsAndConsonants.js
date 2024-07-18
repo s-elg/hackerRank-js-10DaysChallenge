125eb01dbb2efaa173f0c717b63034bc78b1884f
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2024-07-18 11:47:50 +0300

    Update vowelsAndConsonants.js

--- vowelsAndConsonants.js
+++ vowelsAndConsonants.js
@@ -1,7 +1,10 @@
 function vowelsAndConsonants(s) {
-    let vowels = [];
+    //sesli ve sessiz harfleri tuttuğum iki array açıyorum
+    let vowels = []; 
     let consonants = [];
 
+
+    //for döngüsüyle array üzerinde dönerek sesli ve sessiz harfleri ayırıyorum
     for (let j = 0; j < s.length; j++) {
         if (['a', 'e', 'i', 'o', 'u'].includes(s[j])) {
             vowels.push(s[j]);
@@ -9,9 +12,12 @@ function vowelsAndConsonants(s) {
             consonants.push(s[j]);
         }
     }
-    
+
+    //concat fonksiyonuyla iki arrayi birleştiriyorum
     let allLetters = vowels.concat(consonants);
 
+
+    //aşağıya doğru sıralı istediği için for döngüsüyle tüm harfleri tek tek yazdırıyorum
     for (let i = 0; i < allLetters.length; i++) {
         console.log(allLetters[i]);
     }
